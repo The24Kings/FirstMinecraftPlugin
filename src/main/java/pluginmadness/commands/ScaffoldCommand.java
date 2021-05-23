239dd4db734aa89c417f0017fddfb7532441a82b
24Kings <24kingsunite@gmail.com>
2021-05-22 22:20:49 -0700

    add/remove_player now requires Player Name to function

--- src/main/java/pluginmadness/commands/ScaffoldCommand.java
+++ src/main/java/pluginmadness/commands/ScaffoldCommand.java
@@ -3,10 +3,12 @@ package pluginmadness.commands;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import pluginmadness.PluginMadness;
 import pluginmadness.Scaffold;
 
 public class ScaffoldCommand implements CommandExecutor {
@@ -15,6 +17,7 @@ public class ScaffoldCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if(!(sender instanceof Player)) return true;
         Player player = (Player) sender;
+        World world = player.getWorld();
         Location playerLocation = player.getLocation();
         
         if(label.equalsIgnoreCase("scaffold") && args.length == 0) {
@@ -36,11 +39,21 @@ public class ScaffoldCommand implements CommandExecutor {
                 Scaffold.setMaterial(Material.BLUE_ICE);
                 player.sendMessage(ChatColor.AQUA + "Set Scaffold to 'minecraft:blue_ice'");
             } else if(args[0].equalsIgnoreCase("add_player")) {
-                Scaffold.add(player);
-                player.sendMessage(ChatColor.AQUA + "Added: " + ChatColor.WHITE + player.getName());
+                if(args.length > 1) {
+                    Player selPlayer = PluginMadness.getPlugin().getServer().getPlayer(args[1]);
+                    if(world.getPlayers().contains(selPlayer)) {
+                        Scaffold.add(selPlayer);
+                        player.sendMessage(ChatColor.AQUA + "Added: " + ChatColor.WHITE + player.getName());
+                    } else player.sendMessage(ChatColor.DARK_RED + "Player does not exist!");
+                } else player.sendMessage(ChatColor.DARK_RED + "Requires player name!");
             } else if(args[0].equalsIgnoreCase("remove_player")) {
-                Scaffold.remove(player);
-                player.sendMessage(ChatColor.RED + "Removed: " + ChatColor.WHITE + player.getName());
+                if(args.length > 1) {
+                    Player selPlayer = PluginMadness.getPlugin().getServer().getPlayer(args[1]);
+                    if(world.getPlayers().contains(selPlayer)) {
+                        Scaffold.remove(selPlayer);
+                        player.sendMessage(ChatColor.RED + "Removed: " + ChatColor.WHITE + player.getName());
+                    } else player.sendMessage(ChatColor.DARK_RED + "Player does not exist!");
+                } else player.sendMessage(ChatColor.DARK_RED + "Requires player name!");
             } else player.sendMessage(ChatColor.DARK_RED + "Invalid Syntax!");
         }
         return true;
--- src/main/java/pluginmadness/commands/ScaffoldTabCompleter.java
+++ src/main/java/pluginmadness/commands/ScaffoldTabCompleter.java
@@ -1,8 +1,10 @@
 package pluginmadness.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,9 @@ public class ScaffoldTabCompleter implements TabCompleter {
         parameters.add("blue_ice");
         parameters.add("add_player");
         parameters.add("remove_player");
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            parameters.add(p.getName());
+        }
 
         return parameters;
     }
